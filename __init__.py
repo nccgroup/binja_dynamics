512285df411354ac84b5b844eafdc3fff8f02161
Eric Hennenfent <Eric.Hennenfent@nccgroup.trust>
2017-05-30 17:19:33 -0500

    Paramaterized icon size, added binjatron calls

--- __init__.py
+++ __init__.py
@@ -1,15 +1,12 @@
 from binja_toolbar import add_image_button
 from binja_spawn_terminal import spawn_terminal
+from binjatron import run_binary, step_one, step_over, step_out, continue_exec
 
-def term_wrapper(bv):
-    spawn_terminal("lldb " + bv.file.filename)
+iconsize = (24, 24)
 
-def not_implemented(_bv):
-    print("Not implemented!")
-
-add_image_button(".binaryninja/plugins/binja_voltron_toolbar/icons/terminal.png", (32,32), term_wrapper, "Open a terminal with an LLDB Session")
-add_image_button(".binaryninja/plugins/binja_voltron_toolbar/icons/run.png", (32,32), not_implemented, "Run Binary")
-add_image_button(".binaryninja/plugins/binja_voltron_toolbar/icons/stepinto.png", (32,32), not_implemented, "Step to next instruction")
-add_image_button(".binaryninja/plugins/binja_voltron_toolbar/icons/stepover.png", (32,32), not_implemented, "Step over call instruction")
-add_image_button(".binaryninja/plugins/binja_voltron_toolbar/icons/continue.png", (32,32), not_implemented, "Continue to next breakpoint")
-add_image_button(".binaryninja/plugins/binja_voltron_toolbar/icons/stop.png", (32,32), not_implemented, "Stop the binary")
+add_image_button(".binaryninja/plugins/binja_voltron_toolbar/icons/terminal.png", iconsize, lambda bv: spawn_terminal("lldb " + bv.file.filename), "Open a terminal with an LLDB Session")
+add_image_button(".binaryninja/plugins/binja_voltron_toolbar/icons/run.png", iconsize, run_binary, "Run Binary")
+add_image_button(".binaryninja/plugins/binja_voltron_toolbar/icons/stepinto.png", iconsize, step_one, "Step to next instruction")
+add_image_button(".binaryninja/plugins/binja_voltron_toolbar/icons/stepover.png", iconsize, step_over, "Step over call instruction")
+add_image_button(".binaryninja/plugins/binja_voltron_toolbar/icons/finish.png", iconsize, step_out, "Step out of stack frame")
+add_image_button(".binaryninja/plugins/binja_voltron_toolbar/icons/continue.png", iconsize, continue_exec, "Continue to next breakpoint")

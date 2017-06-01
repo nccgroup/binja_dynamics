02b81c5fab1e10d70f19a96d93bbfe73124ae679
Eric Hennenfent <Eric.Hennenfent@nccgroup.trust>
2017-06-01 16:23:50 -0500

    Really hacky message box implementation

--- __init__.py
+++ __init__.py
@@ -16,12 +16,16 @@ reglist = []
 def show_register_window(bv):
     global reglist, main_window
     regs = OrderedDict()
+    # TODO: Find a less stupid way to get the instruction pointer. (Why doesn't binja have this already?)
+    reglist.append('rip')
+    regs['rip'] = (0, 64)
     for reg in filter(lambda x: 'mm' not in x and 'st' not in x and 'base' not in x, bv.arch.full_width_regs):
         regs[reg] = (0, bv.arch.regs[reg].size * 8)
         reglist.append(reg)
 
-    app = QApplication.instance()
-    main_window = [x for x in app.allWidgets() if x.__class__ is QMainWindow][0]
+    if main_window is none:
+        app = QApplication.instance()
+        main_window = [x for x in app.allWidgets() if x.__class__ is QMainWindow][0]
     main_window.regwindow = Window()
     main_window.regwindow.update_registers(regs)
     main_window.regwindow.show()
@@ -41,10 +45,22 @@ def update_wrapper(wrapped, view):
     wrapped(view)
     update_registers(get_registers(view))
 
-PluginCommand.register("Show Register Window", "", show_register_window)
+def enable_dynamics(view):
+    show_register_window(view)
+
+PluginCommand.register("Enable Dynamics", "Enables features for dynamic analysis", enable_dynamics)
 add_image_button(".binaryninja/plugins/binja_voltron_toolbar/icons/terminal.png", iconsize, lambda bv: spawn_terminal("lldb " + bv.file.filename), "Open a terminal with an LLDB Session")
 add_image_button(".binaryninja/plugins/binja_voltron_toolbar/icons/run.png", iconsize, run_binary, "Run Binary")
 add_image_button(".binaryninja/plugins/binja_voltron_toolbar/icons/stepinto.png", iconsize, partial(update_wrapper, step_one), "Step to next instruction")
 add_image_button(".binaryninja/plugins/binja_voltron_toolbar/icons/stepover.png", iconsize, partial(update_wrapper, step_over), "Step over call instruction")
 add_image_button(".binaryninja/plugins/binja_voltron_toolbar/icons/finish.png", iconsize, partial(update_wrapper, step_out), "Step out of stack frame")
 add_image_button(".binaryninja/plugins/binja_voltron_toolbar/icons/continue.png", iconsize, continue_exec, "Continue to next breakpoint")
+
+from message_box import MessageBox
+if main_window is None:
+    app = QApplication.instance()
+    main_window = [x for x in app.allWidgets() if x.__class__ is QMainWindow][0]
+
+main_window.messagebox = MessageBox()
+main_window.messagebox.show()
+main_window.messagebox.update("Different Stuff, possibly much longer than before")
---
+++ message_box/__init__.py
@@ -0,0 +1,26 @@
+from PyQt5 import QtWidgets
+from PyQt5.QtCore import Qt, QByteArray
+from PyQt5 import QtGui
+
+class MessageBox(QtWidgets.QWidget):
+
+    def __init__(self, text="Loading..."):
+        super(MessageBox, self).__init__()
+
+        self.setLayout(QtWidgets.QHBoxLayout())
+        self._layout = self.layout()
+
+        print(QtGui.QMovie.supportedFormats())
+        self._gif = QtWidgets.QLabel()
+        movie = QtGui.QMovie("loading.gif")
+        self._gif.setMovie(movie)
+        movie.start()
+        self._layout.addWidget(self._gif)
+
+        self._message = QtWidgets.QLabel()
+        self._message.setText(text)
+        self._layout.addWidget(self._message)
+        self.setObjectName('Message_Window')
+
+    def update(self, newtext):
+        self._message.setText(newtext)

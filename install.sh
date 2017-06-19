dd05fb75feab9dedc8c38fdcfd0f3b5065a819b2
Eric Hennenfent <Eric.Hennenfent@nccgroup.trust>
2017-06-19 11:11:10 -0500

    Add super basic install script

---
+++ install.sh
@@ -0,0 +1,11 @@
+sudo apt update
+sudo apt install git lldb gdb
+sudo apt install python-pyqt5 python-pip python-dev python-apt
+sudo -H pip install --upgrade pip
+sudo -H pip install -r ~/.binaryninja/plugins/binja_voltron_toolbar/requirements.txt
+
+cd ~/.binaryninja/plugins
+git clone https://github.com/ehennenfent/binja_toolbar.git
+git clone https://github.com/ehennenfent/binja_spawn_terminal.git
+git clone https://github.com/ehennenfent/binjatron.git
+git clone https://github.com/ehennenfent/hexview.git
